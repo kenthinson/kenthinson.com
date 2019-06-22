f82aa32af1fa175d591945537823c71689f6e556
Kent Hinson <kent@kenthinson.com>
2019-06-22 00:40:21 -0500

    rebase home on page template

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,49 +1,45 @@
 import 'package:flutter_web/material.dart';
-
+import 'PageTemplate.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-              body: Container(
-                constraints: BoxConstraints(minWidth: 100000000),
-          color: Color.fromARGB(2255, 21, 30, 41),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: <Widget>[
-            Container(
-              constraints: BoxConstraints(minWidth: 700,minHeight: 400),
-              child: FittedBox(
-                      child: Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                "Hi,",
-                                style: TextStyle(color: Color.fromARGB(255, 102, 252, 241), fontFamily: 'OverpassMono'),
-                              ),
-                              Row(
-                                children: <Widget>[
-                                  Text(
-                                    "I'm ",
-                                    style: TextStyle(
-                                        color: Color.fromARGB(255, 102, 252, 241), fontFamily: 'OverpassMono'),
-                                  ),
-                                  Text(
-                                "Kent,",
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 102, 252, 241), fontFamily: 'Monoton'),
-                              ),
-                                ],
-                              ),
-                              Text(
-                                "a Software Developer.",
-                                style: TextStyle(color: Color.fromARGB(255, 102, 252, 241),fontFamily: 'OverpassMono'),
-                              ),
-                            ],
-                          )))
-            )
-          ],),
-        ),
-      );
+    return pageTemplate(
+        '',
+        FittedBox(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "Hi,",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 102, 252, 241),
+                    fontFamily: 'OverpassMono'),
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "I'm ",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 102, 252, 241),
+                        fontFamily: 'OverpassMono'),
+                  ),
+                  Text(
+                    "Kent,",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 102, 252, 241),
+                        fontFamily: 'Monoton'),
+                  ),
+                ],
+              ),
+              Text(
+                "a Software Developer.",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 102, 252, 241),
+                    fontFamily: 'OverpassMono'),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/PageTemplate.dart
+++ lib/PageTemplate.dart
@@ -1,8 +1,28 @@
 import 'package:flutter_web/cupertino.dart';
 import 'package:flutter_web/material.dart';
 
-  Scaffold pageTemplate(String heading, Widget body) {
-    return Scaffold(
+Scaffold pageTemplate(String heading, Widget body) {
+  buildPage() {
+    List<Widget> newWidgets = [];
+    if (heading != '') {
+      newWidgets.add(Container(
+        constraints: BoxConstraints(minWidth: 500),
+        child: FittedBox(
+            child: Text(
+          heading,
+          style: TextStyle(
+              color: Color.fromARGB(255, 102, 252, 241), fontFamily: 'Monoton'),
+        )),
+      ));
+    }
+    newWidgets.add(Container(
+      constraints: BoxConstraints(minWidth: 500),
+      child: body,
+    ));
+    return newWidgets;
+  }
+
+  return Scaffold(
     body: Container(
       constraints: BoxConstraints(minWidth: 100000000),
       color: Color.fromARGB(2255, 21, 30, 41),
@@ -13,32 +33,16 @@ import 'package:flutter_web/material.dart';
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
-              constraints: BoxConstraints(
-                  minHeight: 400, minWidth: 100, maxWidth: 700),
+              constraints:
+                  BoxConstraints(minHeight: 400, minWidth: 100, maxWidth: 700),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    constraints: BoxConstraints(minWidth: 500),
-                    child: FittedBox(
-                        child: Text(
-                          heading,
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 102, 252, 241),
-                          fontFamily: 'Monoton'),
-                    )),
-                  ),
-                   Container(
-                    constraints: BoxConstraints(minWidth: 500),
-                    child: body,
-                  ),
-                ],
-              ),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: buildPage()),
             )
           ],
         ),
       ),
     ),
   );
-  }
+}
